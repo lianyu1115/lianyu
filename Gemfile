34ac6c7f2d0bd4f1875338db24f76048dc8bb0fa
qqbuby <qqbuby@gmail.com>
2019-07-04 23:07:34 +0800

    Init commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# A sample Gemfile
+source "https://gems.ruby-china.com"
+
+gem "jekyll", "3.8.5"
+
+# If you have any plugins, put them here!
+group :jekyll_plugins do
+    gem "jekyll-paginate", "1.1.0"
+    gem "jekyll-sitemap", "1.2.0"
+    gem "jekyll-feed", "0.9.3"
+end
---
+++ jekyll-post.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env ruby
+# -*- encoding:utf-8 -*-
+#
+# The MIT License (MIT)
+# 
+# Copyright (c) 2018 ROY XU <qqbuby@gmail.com>
+#
+# Description: Create a jekyll post file named 'yyyy-MM-dd-title.md' with front matter.
+#
+# Usage:
+# $ ./jekyll-post.rb -t 'Hello World' -c "['Hello', 'World']"
+# $ cat 2018-04-06-hello-world.md
+# ---
+# layout: post
+# title: Hello World
+# date: 2018-04-06 14:27:23 +0800
+# categories: ['Hello', 'World']
+# tags: ['Hello', 'World']
+# ---
+
+require 'optparse'
+require 'date'
+require 'securerandom'
+
+options = {}
+OptionParser.new do |opts|
+    opts.banner = "Usage: post.rb [options]"
+    opts.on('-t', '--title title', 'The title of the post') { |t| options[:title] = t }
+    opts.on('-c', '--category [\'category1\', \'category2\', .., \'categoryN\']', 'The category of the post') { |c| options[:category] = c }
+    opts.on('-f', '--filename filename', 'The file name of the post without date, default is \'--title\'') { |f| options[:filename] = f }
+    opts.on('--tag [\'tag1\', \'tag2\', .., \'tagN\']', 'The tags of the post') { |tag| options[:tag] = tag }
+    opts.on('-h', '--help', 'Show this help message and exit')  do
+        puts opts
+        exit
+    end
+end.parse!
+
+raise "require the title of the post, '--title'" unless options[:title]
+raise "require the title of the post, '--category'" unless options[:category]
+
+title = options[:title]
+categories = options[:category]
+tags = options[:tag]
+unless tags 
+    tags = categories
+end
+
+filename = options[:filename]
+unless filename 
+    filename = title.dup
+end
+
+date = DateTime.now
+disqus_identifier = SecureRandom::uuid.gsub('-','').hex
+
+filename = filename.gsub(' ', '-').gsub(/\(|\)|\./, '').downcase
+filename = date.strftime('%Y-%m-%d') + '-' + filename + ".md"
+date = date.strftime('%Y-%m-%d %H:%M:%S %z')
+
+front_matter = "---\nlayout: post\ntitle: #{title}\ndate: #{date}\ncategories: #{categories}\ntags: #{tags}\n---"
+
+File.open(filename, 'w') { |f| f.write(front_matter) }
+
+__END__
