0c3fca13c61123ac064882f8a2d8af1ebac2e61f
qqbuby <qqbuby@gmail.com>
2021-01-05 11:09:12 +0800

    Update Google UA to GA4

--- _includes/gtag.js
+++ _includes/gtag.js
@@ -1,11 +1,11 @@
 {% if jekyll.environment == "production" %}
 <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-186395990-1"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WW8P6JTH7J"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-186395990-1');
+  gtag('config', 'G-WW8P6JTH7J');
 </script>
 {% endif %}
