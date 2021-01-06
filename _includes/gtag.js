429c404c2746e53dde514db64d4d79b2484c4acf
qqbuby <qqbuby@gmail.com>
2021-01-06 11:57:43 +0800

    Refine oode layout

--- _includes/gtag.js
+++ _includes/gtag.js
@@ -1,7 +1,7 @@
 {% if jekyll.environment == "production" %}
 <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WW8P6JTH7J"></script>
-    <script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WW8P6JTH7J"></script>
+<script>
       window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
